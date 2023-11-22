4bc0fe4c45d3e4eb075876cf3fad29f8efef7a77
emilplane <emilplane@gmail.com>
2023-11-22 13:53:20 -0600

    console fix

--- scripts/clans.js
+++ scripts/clans.js
@@ -262,7 +262,7 @@ async function getPlayerHOMData(playerNumber) {
 async function getData(url) {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data)
+    //console.log(data)
     return data
 }
 
