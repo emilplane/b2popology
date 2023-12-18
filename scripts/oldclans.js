77107b1d5959a8c8227db77021b77feb52fcd8ea
emilplane <129400320+emilplane@users.noreply.github.com>
2023-12-18 18:43:19 +0000

    wars update

---
+++ scripts/oldclans.js
@@ -0,0 +1,416 @@
+const playerAPILinks = {
+    "emilplane": "https://data.ninjakiwi.com/battles2/users/9fbe138bdd94ada419148a480c74e576c95649ee9911de3a",
+    "DestructivForce": "https://data.ninjakiwi.com/battles2/users/9cec1289dbc5f0a44c4388495b77b425ce5619e99c16d16e",
+    "Vuldora": "https://data.ninjakiwi.com/battles2/users/9fbe10d98c9ef8a211108d4a5a22e5769a0d1ab99a42d839",
+    "redlaserbm": "https://data.ninjakiwi.com/battles2/users/9cea16888e90faa54c16de425825e77fcd054fb89912d030",
+    "Fuzzy": "https://data.ninjakiwi.com/battles2/users/9cba1382dec2adf14e16dd4d5f7be072cd0148bdcd44d93a",
+    "J_Paperz": "https://data.ninjakiwi.com/battles2/users/9ced1188d892f9a11145d94c5c70b472cf071db5cb19db6e",
+    "Professor_Gobbles": "https://data.ninjakiwi.com/battles2/users/9ce941ddd8c3afa61f44874b5876e77ec8561ebe9a15db6b",
+    "Minimum": "https://data.ninjakiwi.com/battles2/users/9cea4188d992aff04a14de495c76e57499041cee9d13dc3c",
+    "fishh": "https://data.ninjakiwi.com/battles2/users/9fba15df8c91acf04b40dd4b0977e724ce561fbfcf46896c",
+    "Data": "https://data.ninjakiwi.com/battles2/users/9cea418c8b9ffda41844864a5622e5729e0648b9cb158b31",
+    "Pugmaster706": "https://data.ninjakiwi.com/battles2/users/9cb7168f8c90f1a14c16db4a5a26e175cc5019bfcf448e3d",
+    "Isaac": "https://data.ninjakiwi.com/battles2/users/9fbe45d88a9faaf04947dc48587be1749a0119bd9b108c3d",
+    "Ava": "https://data.ninjakiwi.com/battles2/users/9fbb16838e92fca31e418a1f0926ef25c45515e8cc138c6b",
+    "JazzyJonah": "https://data.ninjakiwi.com/battles2/users/9cec168edfc4fba34d138a4d5c21e023ce5118be9e148a6c",
+    "Fishylol": "https://data.ninjakiwi.com/battles2/users/9fbe4283d896fbac4a4a8f190922e272cc5714bc9c19dc30",
+    "TheOneAndOnlyGhast": "https://data.ninjakiwi.com/battles2/users/9ce9468e8b92f0a24a448b4b0b20e523ce514eeecd12d06c",
+    "Mathlord": "https://data.ninjakiwi.com/battles2/users/9cb81fde8d91aaa51f428f4b5a70e073ca574ce89718de69",
+    "Waby": "https://data.ninjakiwi.com/battles2/users/9fbe468dd99ffba41f45d94c0d7ae621c9051be8cc19df3b",
+    "Cogareen": "https://data.ninjakiwi.com/battles2/users/9fbf15828ac4fea011408f4a5771e771c8511bed9e43d13e",
+    "Ecolisz": "https://data.ninjakiwi.com/battles2/users/9cec43da8990f8a61d408e425825b724c80d1fef9c41d831",
+    "Krunker Prime": "https://data.ninjakiwi.com/battles2/users/9cee1e82d792fcf71041d9195c73b371cc051fbd96468d3d",
+    "Onion": "https://data.ninjakiwi.com/battles2/users/9fbe468d8e95aaad4911dc190b7ae376cc514fef99188b31",
+    "Coolix": "https://data.ninjakiwi.com/battles2/users/9cea178adbc7f8a71a478e4e5820e7729f501defcb15da3d",
+    "luc1aonstation": "https://data.ninjakiwi.com/battles2/users/9ced16d88a96fff11140da1d5b26e326ce5715be96458b3f",
+    "LockableTiger": "https://data.ninjakiwi.com/battles2/users/9fbe16898e95f1f34a448b490a27e575cc001aba9f16dd6b"
+}
+
+const playerRoster = [
+    {
+        "name": "emilplane",
+        "dateJoined": "11/20/2023",
+        "rank": "Leader"
+    },
+    {
+        "name": "DestructivForce",
+        "dateJoined": "11/20/2023",
+        "rank": "Officer"
+    },
+    {
+        "name": "JazzyJonah",
+        "dateJoined": "11/20/2023",
+        "rank": "Officer"
+    },
+    {
+        "name": "Vuldora",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "redlaserbm",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Fuzzy",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "TheTrustMelon",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "J_Paperz",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Professor_Gobbles",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Minimum",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "fishh",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Data",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Coolix",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Pugmaster706",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Ecolisz",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "TheOneAndOnlyGhast",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Cogareen",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Waby",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Isaac",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Ava",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Fishylol",
+        "dateJoined": "11/20/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Windwire",
+        "dateJoined": "11/21/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Mathlord",
+        "dateJoined": "11/21/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Krunker Prime",
+        "dateJoined": "11/21/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "LockableTiger",
+        "dateJoined": "11/21/2023",
+        "rank": "Member"
+    },
+    {
+        "name": "Onion",
+        "dateJoined": "11/23/2023",
+        "rank": "Backup Member"
+    },
+    {
+        "name": "luc1aonstation",
+        "dateJoined": "11/20/2023",
+        "rank": "Backup Member"
+    },
+]
+
+const optimizedNames = [
+    ["DartMonkey", "Dart Monkey"],
+    ["BoomerangMonkey", "Boomerang Monkey"],
+    ["BombShooter", "Bomb Shooter"],
+    ["TackShooter", "Tack Shooter"],
+    ["IceMonkey", "Ice Monkey"],
+    ["GlueGunner", "Glue Gunner"],
+    ["SniperMonkey", "Sniper Monkey"],
+    ["MonkeySub", "Monkey Sub"],
+    ["MonkeyBuccaneer", "Monkey Buccaneer"],
+    ["MonkeyAce", "Monkey Ace"],
+    ["HeliPilot", "Heli Pilot"],
+    ["MortarMonkey", "Mortar Monkey"],
+    ["DartlingGunner", "Dartling Gunner"],
+    ["WizardMonkey", "Boomerang"],
+    ["SuperMonkey", "Boomerang"],
+    ["NinjaMonkey", "Boomerang"],
+    ["Alchemist", "Alchemist"],
+    ["Druid", "Druid"],
+    ["MonkeyVillage", "Monkey Village"],
+    ["BananaFarm", "Banana Farm"],
+    ["SpikeFactory", "Spike Factory"],
+    ["EngineerMonkey", "Engineer Monkey"],
+
+    ["banana_depot_scene", "Banana Depot"],
+    ["basalt_columns", "Basalt Columns"],
+    ["bloontonium_mines", "Bloontonium Mines"],
+    ["building_site_scene", "Building Site"],
+    ["bloon_bot_factory", "Bloon Bot Factory"],
+    ["basalt_columns", "Basalt Columns"],
+    ["castle_ruins", "Castle Ruins"],
+    ["cobra_command", "Cobra Command"],
+    ["dino_graveyard", "Dino Graveyard"],
+    ["garden", "Garden"],
+    ["glade", "Glade"],
+    ["inflection", "Inflection"],
+    ["koru", "Koru"],
+    ["oasis", "Oasis"],
+    ["off_tide", "Offtide"],
+    ["ports", "Ports"],
+    ["sands_of_time", "Sands Of Time"],
+    ["star", "Star"],
+    ["thin_ice", "Thin Ice"],
+    ["pirate_cove", "Pirate Cove"],
+    ["precious_space", "Precious Space"],
+
+    ["Quincy", "Quincy"],
+    ["Quincy_Cyber", "Cyber Quincy"],
+    ["Gwendolin", "Gwendolin"],
+    ["Gwendolin_Science", "Science Gwendolin"],
+    ["Obyn", "Obyn"],
+    ["Obyn_Ocean", "Ocean Obyn"],
+    ["StrikerJones", "Striker Jones"],
+    ["StrikerJones_Biker", "Biker Bones"],
+    ["Churchill", "Captain Churchill"],
+    ["Churchill_Sentai", "Sentai Churchill"],
+    ["Benjamin", "Benjamin"],
+    ["Benjamin_DJ", "DJ Benjammin'"],
+    ["Ezili", "Ezili"],
+    ["Ezili_SmudgeCat", "Smudge Catt Ezili"],
+    ["PatFusty", "Pat Fusty"],
+    ["PatFusty_Snowman", "Fusty The Snowman"],
+    ["Jericho", "Agent Jericho"],
+    ["Agent_Jericho", "Agent Jericho"],
+    ["Jericho_Highwayman", "Highwayman Jericho"],
+    ["Jericho_StarCaptain", "Star Captain Jericho"],
+
+    ["GuildWar", "Clan War"]
+]
+
+function createStyleElement(id, content) {
+    var style = document.createElement("style");
+    style.type = "text/css";
+    style.id = id;
+    if (style.styleSheet) {
+        style.styleSheet.cssText = content;
+    } else {
+        style.appendChild(document.createTextNode(content));
+    }
+    return style;
+ }
+
+function appendStyleSheet(id, content) {
+    if (!document.querySelector("#" + id)) {
+        var head = document.head || document.getElementsByTagName("head")[0];
+        head.appendChild(createStyleElement(id, content));
+    }
+ }
+
+function getOptimizedName(name) {
+    for (let number in optimizedNames) {
+        if (optimizedNames[number][0] == name) {return optimizedNames[number][1]}
+    }
+    return name
+}
+
+async function getPlayerHOMData(playerNumber) {
+    const response = await fetch(playerAPILinks[playerRoster[playerNumber].name]);
+    const data = await response.json();
+    console.log(data)
+    return data
+}
+
+async function getData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.log(data)
+    return data
+}
+
+async function clanMemberHTML(playerNumber) {
+    let playerMoreInfoButtonHTML = ``
+    let playerPFPHTML = ``
+    let bannedHTML = ``
+    if (playerRoster[playerNumber].banned) {
+        bannedHTML = `<h6 class="redText">Banned</h6>`
+    }
+    if (playerAPILinks[playerRoster[playerNumber].name] != undefined) {
+        let playerData = await getPlayerHOMData(playerNumber)
+        playerMoreInfoButtonHTML = `<span class="material-symbols-outlined" id="${playerNumber}PlayerButton">arrow_outward</span>`
+        playerPFPHTML = `<div class="playerPFP" style="background-image:url(${playerData.body.equippedAvatarURL})"></div>`
+    }
+    return `<div class="clanRosterList">
+        <div style="display: flex; align-items: center; gap: 12px;">
+            ${playerPFPHTML}
+            <h5>${playerRoster[playerNumber].name}</h5>
+            ${bannedHTML}
+            ${playerMoreInfoButtonHTML}
+            <div id="player${playerNumber}Loading"></div>
+        </div>
+    </div>`
+}
+//equippedAvatarURL
+async function updatePlayerWindow(playerNumber) {
+    let playerData = await getPlayerHOMData(playerNumber)
+    document.getElementById("playerDataWindowName").innerText = playerData.body.displayName
+    document.getElementById("playerDataWindowOfficialName").innerText = playerRoster[playerNumber].name
+    document.getElementById("dateJoined").innerText = playerRoster[playerNumber].dateJoined
+    document.getElementById("rank").innerText = playerRoster[playerNumber].rank
+    document.getElementById("b2lolButton").setAttribute("onclick", "window.open('https://b2.lol/playerInfo/playerInfo.html?" + playerAPILinks[playerRoster[playerNumber].name] + "', '_blank');");
+    document.getElementById("avatar").innerHTML = `<div class="bigPlayerPFP" style="background-image:url(${playerData.body.equippedAvatarURL})"></div>`
+    document.getElementById("trophies").innerText = playerData.body.currentSeason_trophies
+    document.getElementById("clubMember").innerText = playerData.body.is_club_member
+    let badgeHTML = ``
+    for (let badgeNumber in playerData.body.badges_all) {
+        badgeHTML += `
+            <div class="playerDataBox badgeBox">
+                <h6>Season ${playerData.body.badges_all[badgeNumber].season}</h6>
+                <img src="${playerData.body.badges_all[badgeNumber].iconURL}"></img>
+            </div>
+        `
+    }
+    document.getElementById("badges").innerHTML = badgeHTML
+    let playerMatchData = await getData(playerData.body.matches)
+    let matchHTML = ``
+    for (let i = 0; i < 5; i++) {
+        try {
+            if (i != undefined && playerMatchData != undefined) {
+                let player1 = await getData(playerMatchData.body[i].playerLeft.profileURL)
+                let player2 = await getData(playerMatchData.body[i].playerRight.profileURL)
+                let opponentName = `player`
+                let winStatus = undefined
+                if (player1.body.displayName == playerData.body.displayName) {
+                    opponentName = player2.body.displayName
+                    winStatus = playerMatchData.body[i].playerLeft.result
+                }
+                if (player2.body.displayName == playerData.body.displayName) {
+                    opponentName = player1.body.displayName
+                    winStatus = playerMatchData.body[i].playerRight.result
+                }
+                let winString;
+                if (winStatus == "win") {
+                    winString = "Win"
+                } else if (winStatus == "lose") {   
+                    winString = "Loss"
+                } else {
+                    winString = "Unknown"
+                }
+                matchHTML += `
+                    <div class="playerDataBox">
+                        <h6>vs. ${opponentName}</h6>
+                        <p>${getOptimizedName(playerMatchData.body[i].gametype)} Match</p>
+                        <p>${winString} on ${getOptimizedName(playerMatchData.body[i].map)}</p>
+                    </div>
+                `
+            }
+        } catch {}
+    }
+    if (playerMatchData.body.length == 0) {
+        document.getElementById("recentMatchesText").innerText = ""
+    } else {
+        document.getElementById("recentMatchesText").innerText = "Recent Matches"
+    }
+    document.getElementById("matches").innerHTML = matchHTML
+    /*let towersHTML = ``
+    for (let towerNumber in playerData.body._towers) {
+        towersHTML += `<div class="playerDataBox"><h6>${getOptimizedName(playerData.body._towers[towerNumber].type)}</h6><p>${playerData.body._towers[towerNumber].used} used</p></div>`
+    }
+    document.getElementById("towers").innerHTML = towersHTML*/
+    let playerBannerStyle = `
+        <style>
+            .playerCoverImage {
+                background-image: linear-gradient(to top, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0)), url('${playerData.body.equippedBannerURL}');
+                @media (prefers-color-scheme: dark) {
+                    background-image: linear-gradient(to top, rgb(29, 29, 29) 10%, rgba(0, 0, 0, 0)), url('${playerData.body.equippedBannerURL}');
+                }
+            }
+        </style>
+    `
+    document.getElementById("playerBannerStyle").innerHTML = playerBannerStyle
+}
+
+document.getElementById("closeButton").addEventListener("click", () => {
+    document.getElementById("playerDataWindow").close();
+});
+
+async function main() {
+    //for (let playerNumber in playerRoster) {await clanMemberHTML(playerNumber)}
+    for (let playerNumber in playerRoster) {
+        if (playerRoster[playerNumber].rank == "Backup Member") {
+            document.getElementById("backupMembersTitle").innerText = "Backup Members"
+            document.getElementById("backupList").insertAdjacentHTML("beforeend", await clanMemberHTML(playerNumber))
+        } else {
+            document.getElementById("clanRosterList").insertAdjacentHTML("beforeend", await clanMemberHTML(playerNumber))
+        }
+        if (document.getElementById(`${playerNumber}PlayerButton`) != undefined) {
+            async function playerEventListenerFunction() {
+                document.getElementById(`player${playerNumber}Loading`).innerHTML = `<div class="lds-dual-ring"></div>`
+                await updatePlayerWindow(playerNumber)
+                document.getElementById(`player${playerNumber}Loading`).innerHTML = ``
+                document.getElementById("playerDataWindow").showModal();
+            }
+            document.getElementById(`${playerNumber}PlayerButton`).addEventListener("click", playerEventListenerFunction);
+        }
+    }
+
+    let clanData = await getData("https://data.ninjakiwi.com/battles2/guild/9fba12d9d792fba01a13de430a71e421c45215b99d178a3d")
+    let clanWars = await getData(clanData.body.wars)
+    console.log(clanWars)
+}
+
+main()
+
+
+document.getElementById(`menuIcon`).addEventListener("click", function() {
+    document.getElementById("menu").showModal();
+});
+document.getElementById(`menuCloseButton`).addEventListener("click", function() {
+    document.getElementById("menu").close();
+});
