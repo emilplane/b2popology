25b1ac03352231a70be7dd5164413d069c509d8d
emilplane <emilplane@gmail.com>
2023-12-26 01:21:20 -0600

    0.18.0.2
    Added version number to the version history page

--- scripts/version.js
+++ scripts/version.js
@@ -2,17 +2,10 @@ let version = {
     "major": 0,
     "minor": 18,
     "patch": 0,
-    "fix": 1
+    "fix": 2
 }
 
 let versionString;
-if (version.patch == 0) {
-    versionString = `Version ${version.major}.${version.minor} - Beta`
-} else {
-    versionString = `Version ${version.major}.${version.minor}.${version.patch} - Beta`
-}
-
-document.getElementById("version").innerHTML = versionString;
 
 if (version.fix == 0) {
     if (version.patch == 0) {
@@ -24,4 +17,20 @@ if (version.fix == 0) {
     versionString = `${version.major}.${version.minor}.${version.patch}.${version.fix}`
 }
 
-document.getElementById("version").innerHTML = versionString
+try {
+    document.getElementById("version").innerHTML = versionString
+} catch {}
+
+let smallVersionString = ""
+
+if (version.fix != 0) {
+    smallVersionString = `.${version.patch}.${version.fix}`
+} else if (version.patch != 0) {
+    smallVersionString = `.${version.patch}`
+}
+
+try {
+    document.getElementById("versionContainer").innerHTML = `
+        <h2>Version <span class="versionText">${version.major}.${version.minor}</span><span class="minorVersions">${smallVersionString}</span></h2>
+    `
+} catch {}
