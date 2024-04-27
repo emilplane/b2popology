3f083f939ee8f3d779a99c918aeb9dbc0c1389c0
emilplane <emilplane@gmail.com>
2024-04-27 11:42:18 -0500

    0.25.3.1

--- scripts/home.js
+++ scripts/home.js
@@ -12,7 +12,7 @@ menuCloseButton.addEventListener('click', function() {
     menuDialog.close(); 
 });
 
-const startDate = new Date('2024-04-27T17:00:00.000Z');
+const startDate = new Date('2024-04-26T17:00:00.000Z');
 const now = new Date();
 const diffTime = (now - startDate)*-1
 console.log(diffTime)
