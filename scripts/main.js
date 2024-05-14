56b622bb49240a5ed2a777493e5c384bba769065
Vuldora <59207724+Vuldora@users.noreply.github.com>
2024-05-14 11:58:57 +0300

    add 365 days in a row accolade

--- scripts/main.js
+++ scripts/main.js
@@ -10652,6 +10652,12 @@ let data = {
                             <li>Play a game every day for 90 days in a row</li>
                         </ul>
                     </ul>
+		    <ul>
+                        <li>Level 3</li>
+                        <ul>
+                            <li>Play a game every day for 365 days in a row</li>
+                        </ul>
+                    </ul>
                 `
             },
             "noPainNoGain10Lives": {
