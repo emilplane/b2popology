0432d9999a4a68d63b7c06c7a1a212f1b47bf288
Vivi <github@vuldora.com>
2026-07-08 20:39:03 +0300

    add tower selector

--- scripts/popology/main.js
+++ scripts/popology/main.js
@@ -1,17 +1,22 @@
+import { TowerSelector } from './ui/tower-selector.js';
 import { Tower } from '/scripts/popology/classes/tower.js';
 
-const towerView = document.getElementById('tower-view');
+const towerViewContainer = document.getElementById('tower-view');
+const towerSelectorContainer = document.getElementById('tower-selector-container');
 
 const params = new URLSearchParams(document.location.search);
 
 main();
 
 async function main() {
-  const towerId = (params.get("id") != null) ? params.get("id") : "dartMonkey"
+  const towerId = (params.get("id") != null) ? params.get("id") : "dart-monkey"
   const towerPath = (params.get("path") != null) ? params.get("path") : "000"
 
+  const towerSelector = new TowerSelector(towerViewContainer);
+  towerSelectorContainer.append(towerSelector.toHTML());
+
   const tower = await Tower.loadTower(towerId);
-  const towerHTML = await tower.toHTML(towerPath, towerView);
-  towerView.appendChild(towerHTML);
+  const towerHTML = await tower.toHTML(towerPath, towerViewContainer);
+  towerViewContainer.append(towerHTML);
 }
 
---
+++ scripts/popology/ui/tower-selector.js
@@ -0,0 +1,38 @@
+import { Tower } from '../classes/tower.js';
+
+export class TowerSelector {
+
+  constructor(towerViewContainer) {
+    this.towerViewContainer = towerViewContainer;
+    this.towerIds = [
+      'dart-monkey',
+      'tack-shooter',
+      'boomerang-monkey'
+    ];
+  }
+
+  toHTML() {
+    const rootContainer = document.createElement('div');
+    this.towerIds.forEach((id) => {
+      const button = document.createElement('button');
+      const img = document.createElement('img');
+      button.append(img);
+
+      button.addEventListener('click', async () => {
+        const tower = await Tower.loadTower(id);
+        const towerHTML = await tower.toHTML('000', this.towerViewContainer);
+        this.towerViewContainer.replaceChildren(towerHTML);
+        history.replaceState(
+          { id },
+          "",
+          `/pages/popology.html?id=${encodeURIComponent(id)}`
+        );
+      });
+      img.src = '/media/tower-portraits/' + id + '/000.png';
+      rootContainer.append(button);
+    });
+
+    return rootContainer;
+  }
+
+}
